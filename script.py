2eb2e6a83e74c97dd57a9ff5aed0e9878c60f263
Vedvardhan <vedvardhan.g@gmail.com>
2022-12-13 15:23:56 +0530

    Fix output design

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
-import mysql.connector as sql
-mycon=sql.connect(host='localhost',user='root',passwd='ved08',database='colleges')           #connectivity established
+import mysql.connector as sqltor
+mycon=sqltor.connect(host='localhost',user='root',passwd='ved08',database='colleges')           #connectivity established
 cur=mycon.cursor()                                                                            #cursor instance created
-      
-      
-      
+           
 print('''
 WELCOME TO THE COLLEGE FINDER PROGRAM!
 ----------------------------------------
-
 Search for the course you want to study from the index below
 and type the associated number.
 \n--------------------------------------------------------------------------------\n''')
+
+
+#COURSES
+
 
 print('''Enter 1 \n  for Bachelor of Science (B.Sc)\n
 Enter 2 \n  for Bachelor of Technology (B.Tech)\n
@@ -24,8 +25,17 @@ Enter 9 \n  for Bachelor of Architecture (B.Arch)\n
 Enter 10 \n  for Chartered Accountancy (C.A)  ''')   
 course=int(input('\nEnter course number:'))
 
-coursedict={1:'B.Sc',2:'B.Tech',3:'B.A',4:'MBBS',5:'B.Com',
-            6:'B.Des',7:'B.F.A',8:'B.B.A',9:'B.Arch',10:'C.A'}
+coursedict={1:'B.Sc',
+            2:'B.Tech',
+            3:'B.A',
+            4:'MBBS',
+            5:'B.Com',
+            6:'B.Des',
+            7:'B.F.A',
+            8:'B.B.A',
+            9:'B.Arch',
+            0:'C.A'}
+
 
 #CITIES
 
@@ -64,8 +74,10 @@ citydict={
     10:'Mumbai',
     11:'Pune',
     12:'Varanasi',
-    13:'Vellore'
+    13:'Vellore',
+    0:'%'
 }
+
 
 #FEE RANGE
 
@@ -73,36 +85,25 @@ citydict={
 print('''\n--------------------------------------------------------------------------------\n
 What is your ideal fee range? Browse in the index below
 and type the associated number.
-
 *Don't worry, there will be scholarship websites provided for each result later!*
 \n--------------------------------------------------------------------------------\n''')
 print('''Enter 1 \n   for <1 Lakhs P.A.\n
-Enter 2 \n  for 1 - 5 Lakhs P.A.\n
-Enter 3 \n  for 5 - 10 Lakhs P.A.\n
-Enter 4 \n  for 10 - 20 Lakhs P.A.\n
+Enter 2 \n  for <5 Lakhs P.A.\n
+Enter 3 \n  for <10 Lakhs P.A.\n
+Enter 4 \n  for <20 Lakhs P.A.\n
 Enter 5 \n  for >20 Lakhs P.A.\n
 Enter 0 \n  TO SKIP QUESTION\n''')
 
 fee=int(input('\nEnter fee range number:'))
-
-feedict={1: [0, 100000], 2: [100000,500000], 3: [500000, 1000000], 4: [1000000, 2000000], 5: [2000000, 9999999999999]}
-fee_range_statement_for_query = None
 if fee == 0:
-    fee_range_statement_for_query = None    
-else:
-    fee_range_statement_for_query = "colleges.fee_range_pa BETWEEN {} AND {}".format(feedict[fee][0], feedict[fee][1])
-
-
-
+    fee = 5
 
 #PERCENTAGE IN CLASS 12
 
 
 print('''\n--------------------------------------------------------------------------------\n
 What was your percentage scored in Class 12 Final Exams (or equivalent)?
-
 Type the numeric value ONLY.
-
 Example:
 Enter Class 12 percentage: 80
 \n--------------------------------------------------------------------------------\n''')
@@ -110,18 +111,15 @@ Enter Class 12 percentage: 80
 percentage=int(input('\nEnter Class 12 percentage:'))
 
 
-#select * from colleges where 
+#CREATING THE QUERY WITH PARAMETERISED VALUES AND EXTRACTING DATA
 
-print("Hello this is my query",fee_range_statement_for_query)
 
-'''query='select * from colleges where Course=%s and City=%s and %s and Percentage_Required<%s;'
+values=(coursedict.get(course),citydict.get(city),fee,percentage)
+print(values)
 
-tuple1=(coursedict.get(course),citydict.get(city),FEE RANGE PART KO UPDATE KARO SAALO,percentage)
-
-use a tuple to store variable values and se statement
-cur.execute(query,tuple1)
+query='select * from colleges where Course like "%{}%" and City like "%{}%" and fee_type<={} and Percentage_Required<{};'.format(coursedict.get(course),citydict.get(city),fee,percentage)
+cur.execute(query)
 data=cur.fetchall()
 for row in data:
     print(row)
 mycon.close()
-'''
